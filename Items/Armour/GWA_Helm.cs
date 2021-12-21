8e29f241e6f2271e20d10ee16891e7b65f7b8558
Salaro <41659951+salaro56@users.noreply.github.com>
2021-12-21 05:34:53 -0700

    GWA
    First set of armour complete, going to bed now

---
+++ Items/Armour/GWA_Helm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using GlockMod.HJD;
+
+namespace GlockMod.Items.Armour
+{
+    [AutoloadEquip(EquipType.Head)]
+    class GWA_Helm : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Gift Wrapped Helmet");
+            Tooltip.SetDefault("2% increased Holly Jolly");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 24;
+            item.height = 22;
+            item.value = Item.buyPrice(0, 0, 2, 50);
+            item.rare = ItemRarityID.Blue;
+            item.defense = 2;
+        }
+
+        public override bool IsArmorSet(Item head, Item body, Item legs)
+        {
+            return body.type == ModContent.ItemType<GWA_Torso>() && legs.type == ModContent.ItemType<GWA_Legs>();
+        }
+
+        public override void UpdateArmorSet(Player player)
+        {
+            player.setBonus = "2 defense";
+            player.statDefense += 2;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            DamagePlayer modPlayer = DamagePlayer.ModPlayer(player);
+            modPlayer.hjdDamageMult *= 1.02f;
+        }
+    }
+}
